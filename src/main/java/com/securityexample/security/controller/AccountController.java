90ba4b210368b0d76b0141b37c750ea564d9bc31
unknown <you@example.com>
2025-09-22 16:16:44 +0530

    Initial comment

---
+++ src/main/java/com/securityexample/security/controller/AccountController.java
@@ -0,0 +1,16 @@
+package com.securityexample.security.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/account")
+@CrossOrigin
+public class AccountController {
+    @GetMapping("/my-account")
+    public String getAccountDetails() {
+        return "Hello account details";
+    }
+}
